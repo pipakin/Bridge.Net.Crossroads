d2c8b08a81f468cf8c5a3d65d1e91d0e521ab5a3
Philip Kin <philip.kin@openlink.com>
2016-03-31 17:15:55 -0500

    Added Hasher bindings.

---
+++ Bridge.Crossroads/Hasher.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge.Crossroads
+{
+    [External]
+    [Namespace(false)]
+    public class Hasher : IDisposable
+    {
+        [Template("hasher")]
+        public static Hasher Instance;
+
+        [Name("VERSION")]
+        public string Version;
+
+        public bool Raw;
+
+        public string AppendHash;
+
+        public string PrependHash;
+
+        public string Separator;
+
+        public Signal<string, string> Changed;
+
+        public Signal<string> Stopped;
+
+        public Signal<string> Initialized;
+
+        public extern void Init();
+
+        public extern void Stop();
+
+        public extern bool IsActive();
+
+        public extern string GetURL();
+
+        public extern string GetBaseURL();
+
+        public extern void SetHash(string hash);
+
+        public extern void ReplaceHash(string hash);
+
+        public extern string GetHash();
+
+        public extern string[] GetHashAsArray();
+
+        public extern void Dispose();
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Bridge.Net bindings for Crossroads.js
 
 Should be fairly self explanatory if you've used Crossroads before.
+
+Includes very basic bindings for Hasher and Signals.
